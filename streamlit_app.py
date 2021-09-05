cca0eb8939a47687c1fe88754bcfbddcd4b83009
kez4twez <tomdweekes@gmail.com>
2021-09-05 21:13:05 +1000

    Added rsi.py and the necessary code to run it in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import lstm
 import ichimoku
+import rsi
 from trading import PortfolioManager
 from alpaca_trade_api.rest import REST
 from PIL import Image
@@ -29,29 +30,29 @@ st.set_page_config(
 # ! Trading Sidebar - Start 
 # TODO: Change the below key to your own API key or Make sure you have set your environment variables
 
-alpaca_api_key = os.getenv("APCA_API_KEY_ID")
-alpaca_secret_key = os.getenv("APCA_API_SECRET_KEY")
-request = requests.get("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=R298VOBAB51H5V8O").json()
-btc_price = round(float(request['Realtime Currency Exchange Rate']['5. Exchange Rate']), 2)
-api = REST(alpaca_api_key, alpaca_secret_key, api_version='v2')
-manager = PortfolioManager()
+# alpaca_api_key = os.getenv("APCA_API_KEY_ID")
+# alpaca_secret_key = os.getenv("APCA_API_SECRET_KEY")
+# request = requests.get("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=R298VOBAB51H5V8O").json()
+# btc_price = round(float(request['Realtime Currency Exchange Rate']['5. Exchange Rate']), 2)
+# api = REST(alpaca_api_key, alpaca_secret_key, api_version='v2')
+# manager = PortfolioManager()
 
-with st.sidebar.form(key ='execution form'):
-    title = st.title('💸 Execute Trade Now!')
-    my_account = st.header("Equity: $"+api.get_account().equity)
-    my_buying_power = st.subheader("Buying Power: $"+api.get_account().buying_power)
-    divider = st.markdown("---")
-    bitcoin_icon = st.image("bitcoin.gif")
-    bitcoin_price = st.metric(label="Current Price in $USD", value = btc_price)
-    bitcoin_qty = st.number_input("Enter Quantity in BTC", value =0.10, min_value = 0.0, max_value = 10.0)    
-    bitcoin_side = st.selectbox("Do you wish to buy or sell bitcoin?", ("buy", "sell"))
-    submit = st.form_submit_button(label = 'Place Market Order Now ✅')
+# with st.sidebar.form(key ='execution form'):
+#     title = st.title('💸 Execute Trade Now!')
+#     my_account = st.header("Equity: $"+api.get_account().equity)
+#     my_buying_power = st.subheader("Buying Power: $"+api.get_account().buying_power)
+#     divider = st.markdown("---")
+#     bitcoin_icon = st.image("bitcoin.gif")
+#     bitcoin_price = st.metric(label="Current Price in $USD", value = btc_price)
+#     bitcoin_qty = st.number_input("Enter Quantity in BTC", value =0.10, min_value = 0.0, max_value = 10.0)    
+#     bitcoin_side = st.selectbox("Do you wish to buy or sell bitcoin?", ("buy", "sell"))
+#     submit = st.form_submit_button(label = 'Place Market Order Now ✅')
 
-    if submit: 
-        post_url = "https://paper-api.alpaca.markets/v2/orders"
-        post_json = {"symbol":"TSLA", "qty":bitcoin_qty, "side":bitcoin_side, "type":"market", "time_in_force":"day"}
-        post_header = {"APCA-API-KEY-ID":alpaca_api_key , "APCA-API-SECRET-KEY":alpaca_secret_key}
-        response = requests.post(post_url, json = post_json, headers= post_header)
+#     if submit: 
+#         post_url = "https://paper-api.alpaca.markets/v2/orders"
+#         post_json = {"symbol":"TSLA", "qty":bitcoin_qty, "side":bitcoin_side, "type":"market", "time_in_force":"day"}
+#         post_header = {"APCA-API-KEY-ID":alpaca_api_key , "APCA-API-SECRET-KEY":alpaca_secret_key}
+#         response = requests.post(post_url, json = post_json, headers= post_header)
 
 # ! Trading Sidebar - End
 
@@ -135,9 +136,17 @@ for whale in range(5):
 """
 ---
 # 💹 RSI Trading Strategy
-
+RSI stands for Relative Strength Index. It is a popular indicator for trading. When an assets RSI is below 30 it is considered oversold and a buy signal. When an assets RSI is above 70 it is considered overbought and a sell signal.
+\n Below you will see a price feed for ETHUSD and the current RSI, when it is either overbought or oversold a signal will appear.
 """
 
+chart_container = st.container()
+price_container = st.container()
+with chart_container:
+    rsi.plot_chart()
+
+with price_container:
+    rsi.get_rsi_price()
 
 
 """
